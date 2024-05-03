39605a665a59bce73328878a1d697ade7ca4e2b6
MeetMuaz <muazbalogun97@gmail.com>
2024-05-03 18:23:14 +0100

    Hello World!

---
+++ Hello.java
@@ -0,0 +1,23 @@
+class Hello 
+{
+    public static void main (String a[])
+    {
+        // int num1 = 3;
+        // int num2 = 2;
+        // int result = num1 + num2;
+        // System.out.println(result);
+
+        int num1 =9;
+        byte by = 127;
+        short sh = 558;
+        long lg = 5854l;
+
+        float l = 5.8f;
+        double d = 5.8;
+
+        char c = 'k'; //literals
+
+        boolean b = true;
+    }
+    
+}
